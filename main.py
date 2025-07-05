a67f74708ba916d3ff00d58c19baab49a23e1511
Krishang Gupta <krishanggupta.kg@gmail.com>
2025-07-05 22:14:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import model
+
+app = Flask("__name__")
+
+@app.get("/")
+def ping():
+    return "This site is up !!!"
+
+@app.post("/sentiment-analysis/train")
+def train_model():
+    params = request.get_json()
+    return params
+
+@app.post("/sentiment-analysis/predict")
+def predict_sentiment():
+    params = request.get_json()
+    result  = model.predict(params)
+    return result
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5004)
